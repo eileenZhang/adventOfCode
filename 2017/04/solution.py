22b49e631a8bd9008c090a507253db43ac1e51b7
eileenZhang <eileen.zhang@uwaterloo.ca>
2017-12-04 22:01:09 -0500

    2017 day 4

---
+++ 2017/04/solution.py
@@ -0,0 +1,21 @@
+
+
+def solution(function):
+    passwords = open("input.txt").read().split("\n")
+    count = 0
+    for password in passwords:
+        line = password.split(" ")
+        dup = set()
+        valid = True
+        for word in line:
+            new_word = function(word)
+            if new_word in dup:
+                valid = False
+            else: 
+                dup.add(new_word)
+        if valid:
+            count += 1
+    return count
+           
+print "part1:", solution(lambda string: string)
+print "part1:", solution(lambda string: "".join(sorted(string)))
